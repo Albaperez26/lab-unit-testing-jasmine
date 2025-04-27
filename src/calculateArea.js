e6d69eb2e3e172b63ddc2e08af44ec8ab61e65f0
Alba Perez <albaperezbaldo@gmail.com>
2025-04-27 19:09:37 +0200

    lab done

--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,12 @@
-function calculateArea() {
+function calculateArea(x, y) {
+    if (x == undefined || y == undefined) {
+        return undefined;
+    }
+
+    if (typeof x != "number" || typeof y != "number"){
+        return undefined;
+    }
+
+    return x * y;
 
 }
